67e0b8f1506290f83b81ce9905d0a57fcd6e09d6
xach3110 <kazerog543@gmail.com>
2025-05-19 09:19:44 +0200

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { initScrollReveal } from './utils/scrollReveal';
 
 import { HeroSection } from './components/first';
